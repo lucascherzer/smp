0ac6d8880ff88577ca30b0c2886828fc59a979f7
Luca Scherzer <83914554+lucascherzer@users.noreply.github.com>
2023-03-26 20:17:41 +0200

    add argparse and README

---
+++ README.md
@@ -0,0 +1,17 @@
+# sitemap parser
+
+A cli script to parse the usual sitemap resources of a website (e.g. `robots.txt`, `sitemap.xml`, etc)
+
+This is really just a front end for the `ultimate-sitemap-parser`, so all credit to them!
+
+## Usage
+1. Initialize a venv (optional)
+```sh
+python3 -m venv .venv
+pip3 install -r requirements.txt
+```
+2. Run
+```sh
+python3 smp.py "<target url>"
+# Get help with the -h flag
+```
--- smp.py
+++ smp.py
@@ -1,5 +1,42 @@
+import logging
+import argparse
 from usp.tree import sitemap_tree_for_homepage
 
-tree = sitemap_tree_for_homepage('https://string-functions.com')
+PARSER = argparse.ArgumentParser(
+    prog="smp",
+    description="Sitemap Parser"
+    )
+
+PARSER.add_argument("target",
+                    help="Target URL"
+                    )
+
+PARSER.add_argument("-l",
+                    "--loglevel",
+                    required=False,
+                    nargs=1,
+                    choices=["debug","info", "warn", "error", "critical"],
+                    help="The log level for USP. Default is 'critical'",
+                    default=["critical"]
+                    )
+ARGS = PARSER.parse_args()
+
+
+LOG_LEVELS = {
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warn": logging.WARN,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL
+}
+
+# Change logger config according to args 
+level = LOG_LEVELS[ARGS.loglevel[0]] # ARGS.loglevel is a list but only holds one value
+logging.getLogger("usp.fetch_parse").setLevel(level)
+logging.getLogger("usp.helpers").setLevel(level)
+logging.getLogger("usp.tree").setLevel(level)
+
+
+tree = sitemap_tree_for_homepage(ARGS.target)
 for page in tree.all_pages():
     print(page.url)
